719ffa5779f948e0ab29881e6f524888264b9ef2
Juraci Vieira <juraci.vieira@gmail.com>
2017-03-29 09:31:45 -0300

    less acceptance more integration

--- spec/acceptance/authentication_spec.js
+++ spec/acceptance/authentication_spec.js
@@ -22,40 +22,4 @@ describe('POST /authenticate', () => {
         });
     });
   });
-
-  context('when the user sends invalid password', () => {
-    beforeEach(() => User.create({ email, password }));
-
-    it('returns Authentication failed message', (done) => {
-      const credentials = { email: 'user-sample@sensors.com', password: 'my-wrong-password' };
-      request
-        .post('/authenticate')
-        .send(credentials)
-        .end((err, res) => {
-          expect(res.status).to.equal(200);
-          expect(res.body.success).to.be.equal(false);
-          expect(res.body.message).to.be.equal('Authentication failed');
-          expect(res.body).to.not.have.key('token');
-          done(err);
-        });
-    });
-  });
-
-  context('when the user does not exist', () => {
-    beforeEach(() => User.create({ email, password }));
-
-    it('returns Authentication failed message', (done) => {
-      const credentials = { email: 'wrong-user@sensors.com', password };
-      request
-        .post('/authenticate')
-        .send(credentials)
-        .end((err, res) => {
-          expect(res.status).to.equal(200);
-          expect(res.body.success).to.be.equal(false);
-          expect(res.body.message).to.be.equal('Authentication failed');
-          expect(res.body).to.not.have.key('token');
-          done(err);
-        });
-    });
-  });
 });
--- spec/layers/controllers/users_spec.js
+++ spec/layers/controllers/users_spec.js
@@ -4,19 +4,19 @@ describe('UsersController', () => {
   let datasource;
 
   beforeEach(() => setupDatasource()
-      .then((ds) => {
-        datasource = ds;
-      })
-      .then(() => destroyAll(datasource)));
+    .then((ds) => {
+      datasource = ds;
+    })
+    .then(() => destroyAll(datasource)));
 
   describe('#authenticate', () => {
-    context('valid credentials', () => {
-      const email = 'user-sample@sensors.com';
-      const password = 'my-secret-password';
+    const email = 'user-sample@sensors.com';
+    const password = 'my-secret-password';
 
-      beforeEach(() => datasource.models.User.create({ email, password }));
+    beforeEach(() => datasource.models.User.create({ email, password }));
 
-      it('should authenticate user with valid credentials', () => {
+    context('when the user sends valid credentials', () => {
+      it('returns the JWT', () => {
         const usersController = new UsersController(datasource.models);
         return usersController.authenticate({ email, password })
           .then((result) => {
@@ -25,6 +25,34 @@ describe('UsersController', () => {
               success: true,
               message: 'enjoy your token',
               token: 'auth-token',
+            });
+          });
+      });
+    });
+
+    context('when the user sends the wrong password', () => {
+      it('returns Authentication failed message', () => {
+        const usersController = new UsersController(datasource.models);
+        return usersController.authenticate({ email, password: 'wrong-password' })
+          .then((result) => {
+            expect(result.status).to.be.equal(200);
+            expect(result.data).to.be.deep.equal({
+              success: false,
+              message: 'Authentication failed',
+            });
+          });
+      });
+    });
+
+    context('when the user does not exist', () => {
+      it('returns Authentication failed message', () => {
+        const usersController = new UsersController(datasource.models);
+        return usersController.authenticate({ email: 'wrong-email@test.com', password })
+          .then((result) => {
+            expect(result.status).to.be.equal(200);
+            expect(result.data).to.be.deep.equal({
+              success: false,
+              message: 'Authentication failed',
             });
           });
       });
