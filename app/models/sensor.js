99ed5d4360f3a183c486bc54351b15f4ec316577
Juraci Vieira <juraci.vieira@gmail.com>
2017-03-29 16:54:46 -0300

    adding User hasMany Sensor association

--- app/models/sensor.js
+++ app/models/sensor.js
@@ -5,7 +5,7 @@ module.exports = function (sequelize, DataTypes) {
   }, {
     classMethods: {
       associate(models) {
-        // associations can be defined here
+        Sensor.belongsTo(models.User);
       },
     },
   });
--- app/models/user.js
+++ app/models/user.js
@@ -6,7 +6,7 @@ module.exports = function (sequelize, DataTypes) {
   }, {
     classMethods: {
       associate(models) {
-        // associations can be defined here
+        User.hasMany(models.Sensor, { as: 'sensors' });
       },
     },
   });
