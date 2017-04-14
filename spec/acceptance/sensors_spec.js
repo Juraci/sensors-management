0976f32ad35081a3ded796d11c180236be14fe3f
Juraci Vieira <juraci.vieira@gmail.com>
2017-04-14 17:38:15 -0300

    GET /sensors describe

--- spec/acceptance/sensors_spec.js
+++ spec/acceptance/sensors_spec.js
@@ -1,6 +1,6 @@
 import jwt from 'jsonwebtoken';
 
-describe('GET /sensors', () => {
+describe('sensors', () => {
   const User = app.get('datasource').models.User;
   const Sensor = app.get('datasource').models.Sensor;
   const email = 'user-sample@sensors.com';
@@ -8,13 +8,79 @@ describe('GET /sensors', () => {
 
   beforeEach(done => destroyAll(done));
 
-  context('for an authenticated request', () => {
-    let user;
-    let sensor;
-    let token;
+  describe('GET /sensors', () => {
+    context('for an authenticated request', () => {
+      let user;
+      let sensor;
+      let token;
 
-    beforeEach((done) => {
-      User.create({ email, password })
+      beforeEach((done) => {
+        User.create({ email, password })
+          .then((record) => {
+            user = record;
+          })
+          .then(() => Sensor.create({
+            UserId: user.id,
+            boardId: 'my-board-id',
+            description: 'motion sensor in the living room',
+          }))
+          .then((sensorRecord) => {
+            sensor = sensorRecord;
+          })
+          .then(() => {
+            const credentials = { email, password };
+            request
+              .post('/authenticate')
+              .send(credentials)
+              .end((err, res) => {
+                token = res.body.token;
+                done();
+              });
+          });
+      });
+
+      it('returns the sensors', (done) => {
+        request
+          .get('/sensors')
+          .set('x-access-token', token)
+          .set('Accept', 'application/json')
+          .end((err, res) => {
+            expect(res.status).to.equal(200);
+            expect(res.body.data).to.have.length(1);
+            const record = res.body.data[0];
+            expect(record.type).to.equal('sensors');
+            expect(record.attributes).to.have.all.keys('description', 'board-id');
+            expect(record.attributes['board-id']).to.be.equal(sensor.boardId);
+            done(err);
+          });
+      });
+    });
+
+    context('for an unauthenticated request', () => {
+      beforeEach(() => User.create({ email, password })
+        .then(user => Sensor.create({
+          UserId: user.id,
+          boardId: 'my-board-id',
+          description: 'motion sensor in the living room',
+        })),
+      );
+
+      it('returns 401 unauthorized', (done) => {
+        request
+          .get('/sensors')
+          .set('Accept', 'application/json')
+          .end((err, res) => {
+            expect(res.status).to.equal(401);
+            done();
+          });
+      });
+    });
+
+    context('for a request with a fake token', () => {
+      let fakeToken;
+      let user;
+
+      beforeEach(() => User.create({ email, password })
         .then((record) => {
           user = record;
         })
@@ -23,88 +89,24 @@ describe('GET /sensors', () => {
           boardId: 'my-board-id',
           description: 'motion sensor in the living room',
         }))
-        .then((sensorRecord) => {
-          sensor = sensorRecord;
-        })
         .then(() => {
-          const credentials = { email, password };
-          request
-            .post('/authenticate')
-            .send(credentials)
-            .end((err, res) => {
-              token = res.body.token;
-              done();
-            });
-        });
-    });
+          fakeToken = jwt.sign({
+            id: user.id,
+            email: user.email,
+          }, 'wrongSecret', { expiresIn: '24h' });
+        }),
+      );
 
-    it('returns the sensors', (done) => {
-      request
-        .get('/sensors')
-        .set('x-access-token', token)
-        .set('Accept', 'application/json')
-        .end((err, res) => {
-          expect(res.status).to.equal(200);
-          expect(res.body.data).to.have.length(1);
-          const record = res.body.data[0];
-          expect(record.type).to.equal('sensors');
-          expect(record.attributes).to.have.all.keys('description', 'board-id');
-          expect(record.attributes['board-id']).to.be.equal(sensor.boardId);
-          done(err);
-        });
-    });
-  });
-
-  context('for an unauthenticated request', () => {
-    beforeEach(() => User.create({ email, password })
-      .then(user => Sensor.create({
-        UserId: user.id,
-        boardId: 'my-board-id',
-        description: 'motion sensor in the living room',
-      })),
-    );
-
-    it('returns 401 unauthorized', (done) => {
-      request
-        .get('/sensors')
-        .set('Accept', 'application/json')
-        .end((err, res) => {
-          expect(res.status).to.equal(401);
-          done();
-        });
-    });
-  });
-
-  context('for a request with a fake token', () => {
-    let fakeToken;
-    let user;
-
-    beforeEach(() => User.create({ email, password })
-      .then((record) => {
-        user = record;
-      })
-      .then(() => Sensor.create({
-        UserId: user.id,
-        boardId: 'my-board-id',
-        description: 'motion sensor in the living room',
-      }))
-      .then(() => {
-        fakeToken = jwt.sign({
-          id: user.id,
-          email: user.email,
-        }, 'wrongSecret', { expiresIn: '24h' });
-      }),
-    );
-
-    it('returns unauthorized', (done) => {
-      request
-        .get('/sensors')
-        .set('x-access-token', fakeToken)
-        .set('Accept', 'application/json')
-        .end((err, res) => {
-          expect(res.status).to.equal(401);
-          done();
-        });
+      it('returns unauthorized', (done) => {
+        request
+          .get('/sensors')
+          .set('x-access-token', fakeToken)
+          .set('Accept', 'application/json')
+          .end((err, res) => {
+            expect(res.status).to.equal(401);
+            done();
+          });
+      });
     });
   });
 });
