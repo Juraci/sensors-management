cfbc0ac78e42fcf977b11d45892b07f5275a84eb
Juraci Vieira <juraci.vieira@gmail.com>
2017-04-24 23:35:42 -0300

    lint fix

--- spec/acceptance/alerts_spec.js
+++ spec/acceptance/alerts_spec.js
@@ -46,8 +46,8 @@ describe('sensors', () => {
         });
     });
 
-      it('deletes the alert', (done) => {
-        request
+    it('deletes the alert', (done) => {
+      request
           .del(`/alerts/${alert.id}`)
           .set('x-access-token', token)
           .set('Accept', 'application/json')
@@ -55,24 +55,24 @@ describe('sensors', () => {
             expect(res.status).to.equal(204);
             done(err);
           });
-      });
+    });
 
     context('when a user attempts to delete an alert that he does not own', () => {
       let alert2;
       beforeEach(() => User.create({ email: 'user2@sample.com', password })
-        .then((record) => Sensor.create({
+        .then(record => Sensor.create({
           UserId: record.id,
           boardId: 'my-board-id',
           description: 'motion sensor in the living room',
         }))
-        .then((sensorRecord) => Alert.create({
+        .then(sensorRecord => Alert.create({
           SensorId: sensorRecord.id,
           message: 'Motion detected',
           seen: false,
         }))
         .then((newAlert) => {
           alert2 = newAlert;
-        })
+        }),
       );
 
       it('returns 404', (done) => {
@@ -85,6 +85,6 @@ describe('sensors', () => {
             done(err);
           });
       });
-    })
+    });
   });
 });
